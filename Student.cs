01f838ff3ae884e317e5d0ea07bf2b606971997a
Thanh Dat <datmathoingu@gmail.com>
2020-09-21 22:32:48 +0700

    Initial commit

---
+++ Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StaticMethod
+{
+    public class Student
+    {
+        private int rollno;
+        private string name;
+        private static string college = "BBDIT";
+        public Student(int r, string n)
+        {
+            rollno = r;
+            name = n;
+        }
+        public static void Change()
+        {
+            college = "CODEGYM";
+        }
+        public void Display()
+        {
+            Console.WriteLine(rollno + " " + name + " " + college);
+        }
+    }
+}
